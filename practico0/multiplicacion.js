c9be8c8cc33a779beaef622d93e11609a7cc987e
DevDev05 <ustcapo555@gmail.com>
2023-05-24 00:12:08 -0300

    subiendo practico0

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,8 @@
+let num1 = 2;
+let num2 = 17;
+let num3 = 6;
+let multip = num1 * num2 * num3;
+console.log(multip);
+
+// ¿Qué valor devuelve su programa si los números son 2 * 17 * 6?
+// RTA: 204
---
+++ practico0/resta.js
@@ -0,0 +1,7 @@
+let num1 = 10;
+let num2 = 2;
+let resta = num1 - num2;
+console.log(resta);
+
+// ¿Qué valor devuelve su programa si el minuendo es 10 y el sustraendo 2?
+// RTA: 8
