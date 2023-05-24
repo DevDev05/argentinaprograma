e1a05887d56e3716808d0750275e038c181580db
DevDev05 <ustcapo555@gmail.com>
2023-05-24 00:15:20 -0300

    Agrego el archivo conversionGrados.js

---
+++ conversionGrados.js
@@ -0,0 +1,9 @@
+let gradosF = 75;
+let gradosC = (gradosF - 32) * 5/9;
+console.log(gradosC);
+
+// a. ¿Cuántos grados C son 50 F?
+// RTA: 10 Grados C
+
+// b. ¿Cuántos grados C son 75 F?
+// RTA: 23.88888888888889 Grados C
